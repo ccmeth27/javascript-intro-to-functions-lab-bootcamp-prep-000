1d611ff1c00d2f9b14ee9bec4a5b296ed95f8b82
Connor Meth <connormeth27@gmail.com>
2019-08-03 18:52:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function logWhisper(string) {
   return console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if (logWhisper(string) === "hello")
+  if (string.whisper === "hello")
   	console.log("I can`t hear you")
 	} else if (logShout(string) === "HELLO") {
 	console.log ("YES INDEED")
