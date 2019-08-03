b50a1be9a924fe4086d8e1e4ff10b1ad9c07d7b3
Connor Meth <connormeth27@gmail.com>
2019-08-03 19:03:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,9 +29,9 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === "hello") {
   	return "I can\'t hear you!";
-	} else if ("HELLO") {
+	} else if (string === "HELLO") {
 	return"YES INDEED";
-	} else {
+	} else if (string ==="I love you, Grandma.") {
 	return"I love you, too.";
 	}
 }
